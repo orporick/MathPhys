baa6de4ff44bcf1aecf45936f12c740590e1c653
orporick <riccardo.giannitrapani@gmail.com>
2015-11-07 11:21:18 +0100

    first commit

---
+++ notebooks/mympl.py
@@ -0,0 +1,22 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import show
+
+def hide_spines():
+    """Hides the top and rightmost axis spines from view for all active
+    figures and their respective axes."""
+
+    # Retrieve a list of all current figures.
+    figures = [x for x in matplotlib._pylab_helpers.Gcf.get_all_fig_managers()]
+    for figure in figures:
+        # Get all Axis instances related to the figure.
+        for ax in figure.canvas.figure.get_axes():
+            # Disable spines.
+            ax.spines['right'].set_color('none')
+            ax.spines['top'].set_color('none')
+            # Disable ticks.
+            ax.xaxis.set_ticks_position('bottom')
+            ax.yaxis.set_ticks_position('left')
+            ax.spines['bottom'].set_position(('data', 0))
+            ax.spines['left'].set_position(('data', 0))
